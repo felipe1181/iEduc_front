1c3a0f16e9a9bc24244ac5fb9aef468f9cb2de66
felipe1181 <felipe1181jua@gmail.com>
2020-04-19 09:49:07 -0300

    responsividade do componente de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css';
 
 import { Row, Col, Layout, Form, Input, Button, Checkbox } from 'antd';
-import IconStudent from '../../assets/icon/icon_student_login.png';
+import IconStudent from '../../assets/icons/icon_student_login.png';
 
 function Login() {
     return (
@@ -12,25 +12,30 @@ function Login() {
                     xs={{ span: 22, offset: 1 }}
                     sm={{ span: 20, offset: 2 }}
                     md={{ span: 14, offset: 5 }}
-                    lg={{ span: 11, offset: 7 }}
-                    xl={{ span: 11, offset: 7 }}>
+                    lg={{ span: 13, offset: 6 }}
+                    xl={{ span: 13, offset: 6 }}>
                     <Row className="login-container-content">
-                        <Col className="login-container-form" span={10}>
-                            <center><img src={IconStudent}></img></center>
+                        <Col className="login-container-form"
+                            xs={{ span: 24 }}
+                            sm={{ span: 20 }}
+                            md={{ span: 16 }}
+                            lg={{ span: 12 }}
+                            xl={{ span: 12 }}>
+                            <center><img src={IconStudent} alt="icon-student" ></img></center>
                             <Form
                                 name="basic"
                                 initialValues={{ remember: true }}
                             >
                                 <Form.Item
-                                    name="username"
-                                    rules={[{ required: true, message: 'Please input your username!' }]}
+                                    name="email"
+                                    rules={[{ required: true, message: 'Por favor preencha seu email!' }]}
                                 >
                                     <Input placeholder="Seu email!" />
                                 </Form.Item>
 
                                 <Form.Item
                                     name="password"
-                                    rules={[{ required: true, message: 'Please input your password!' }]}
+                                    rules={[{ required: true, message: 'Por favor preencha sua senha!' }]}
                                 >
                                     <Input.Password placeholder="Sua senha!" />
                                 </Form.Item>
@@ -42,11 +47,16 @@ function Login() {
                                 <Form.Item >
                                     <Button type="primary" htmlType="submit" block>
                                         Manda ai!
-        </Button>
+                                    </Button>
                                 </Form.Item>
                             </Form>
                         </Col>
-                        <Col className="login-container-bg" span={14}> </Col>
+                        <Col className="login-container-bg"
+                            xs={{ span: 0 }}
+                            sm={{ span: 4 }}
+                            md={{ span: 8 }}
+                            lg={{ span: 12 }}
+                            xl={{ span: 12 }}> </Col>
                     </Row>
                 </Col>
             </Row>
